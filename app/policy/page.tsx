843562333b3e0d03421e84b4439d3de63b65f1fd
Silverium <soldeplatadeveloper@gmail.com>
2023-12-04 12:50:06 +0100

    fix formatting

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -1,69 +1,74 @@
-
 const GDPRPolicyPage = () => {
-  return (
-    <div className="container mx-auto px-4 py-16 text-zinc-100">
-      <h1 className="text-3xl font-bold">Privacy Policy</h1>
+	return (
+		<div className="container mx-auto px-4 py-16 text-zinc-100">
+			<h1 className="text-3xl font-bold">Privacy Policy</h1>
 
-      <p className="mt-4">
-        This Privacy Policy governs the processing of your personal data by "Soldeplata Dev".
-         We are committed to protecting your privacy and ensuring that your personal data is handled in a secure and responsible manner.
-      </p>
+			<p className="mt-4">
+				This Privacy Policy governs the processing of your personal data by
+				"Soldeplata Dev". We are committed to protecting your privacy and
+				ensuring that your personal data is handled in a secure and responsible
+				manner.
+			</p>
 
-      <h2 className="mt-8">Information We Collect</h2>
+			<h2 className="mt-8">Information We Collect</h2>
 
-      <p className="mt-4">
-        We collect the following personal data from you when you visit our website or use our services:
-      </p>
+			<p className="mt-4">
+				We collect the following personal data from you when you visit our
+				website or use our services:
+			</p>
 
-      <ul className="list-disc mt-4">
-        <li>Your name and email address</li>
-        <li>Your IP address</li>
-        <li>Your browsing history on our website</li>
-      </ul>
+			<ul className="list-disc mt-4">
+				<li>Your name and email address</li>
+				<li>Your IP address</li>
+				<li>Your browsing history on our website</li>
+			</ul>
 
-      <p className="mt-4">
-        We may also collect additional personal data from you if you voluntarily provide it to us, such as your phone number or mailing address.
-      </p>
+			<p className="mt-4">
+				We may also collect additional personal data from you if you voluntarily
+				provide it to us, such as your phone number or mailing address.
+			</p>
 
-      <h2 className="mt-8">How We Use Your Personal Data</h2>
+			<h2 className="mt-8">How We Use Your Personal Data</h2>
 
-      <p className="mt-4">
-        We use your personal data for the following purposes:
-      </p>
+			<p className="mt-4">
+				We use your personal data for the following purposes:
+			</p>
 
-      <ul className="list-disc mt-4">
-        <li>To provide our services to you</li>
-        <li>To improve our services</li>
-        <li>To communicate with you about our services</li>
-        <li>To comply with legal requirements</li>
-      </ul>
+			<ul className="list-disc mt-4">
+				<li>To provide our services to you</li>
+				<li>To improve our services</li>
+				<li>To communicate with you about our services</li>
+				<li>To comply with legal requirements</li>
+			</ul>
 
-      <h2 className="mt-8">Your Rights</h2>
+			<h2 className="mt-8">Your Rights</h2>
 
-      <p className="mt-4">
-        You have the following rights with respect to your personal data:
-      </p>
+			<p className="mt-4">
+				You have the following rights with respect to your personal data:
+			</p>
 
-      <ul className="list-disc mt-4">
-        <li>The right to access your personal data</li>
-        <li>The right to rectify your personal data</li>
-        <li>The right to erase your personal data</li>
-        <li>The right to restrict the processing of your personal data</li>
-        <li>The right to object to the processing of your personal data</li>
-        <li>The right to data portability</li>
-      </ul>
+			<ul className="list-disc mt-4">
+				<li>The right to access your personal data</li>
+				<li>The right to rectify your personal data</li>
+				<li>The right to erase your personal data</li>
+				<li>The right to restrict the processing of your personal data</li>
+				<li>The right to object to the processing of your personal data</li>
+				<li>The right to data portability</li>
+			</ul>
 
-      <p className="mt-4">
-        To exercise your rights, please contact us at soldeplatadeveloper@gmail.com.
-      </p>
+			<p className="mt-4">
+				To exercise your rights, please contact us at
+				soldeplatadeveloper@gmail.com.
+			</p>
 
-      <h2 className="mt-8">Contact Us</h2>
+			<h2 className="mt-8">Contact Us</h2>
 
-      <p className="mt-4">
-        If you have any questions about this Privacy Policy, please contact us at soldeplatadeveloper@gmail.com.
-      </p>
-    </div>
-  );
+			<p className="mt-4">
+				If you have any questions about this Privacy Policy, please contact us
+				at soldeplatadeveloper@gmail.com.
+			</p>
+		</div>
+	);
 };
 
 export default GDPRPolicyPage;
